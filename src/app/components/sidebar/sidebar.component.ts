fad439a035cf9bfe57d8e8c6597240a5954738b6
joaoVs17 <joaosimao051@gmail.com>
2024-04-26 22:45:06 -0400

    sidebar working

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  topics:any[] = [
+    {
+      title: "Sobre Mim",
+      icon: "pi pi-address-book",
+    },
+    {
+      title: "Projetos",
+      icon: "pi pi-hammer",
+    },
+    {
+      title: "Contato",
+      icon: "pi pi-phone"
+    }
+  ];
+
+  collapsed:Boolean = true;
+
+  toggleCollapse(){
+    this.collapsed = !this.collapsed
+  }
 
 }
